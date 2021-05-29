9de89976b09d6e7339bb66d7e41dfba09da8f250
Peterson <petersonsimiao1@gmail.com>
2021-05-29 19:28:29 -0300

    feat: criado componente Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { GlobalStyle } from "./style/global";
 import { HeaderComponent } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
 
 export function App() {
   return (
     <>
       <GlobalStyle />
       <HeaderComponent />
+      <Dashboard />
     </>
   );
 }
---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Summary } from "../Summary";
+import { Main } from "./styled";
+
+export function Dashboard() {
+  return (
+    <Main>
+      <Summary />
+    </Main>
+  );
+}
